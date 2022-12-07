e2f577ae70aa37ffbb5c6c3a1dc0c069ee478868
Rofin-A <rofin.andonissamy@gmail.com>
2022-12-07 05:40:17 -0500

    completed ci build6

--- service/routes.py
+++ service/routes.py
@@ -77,7 +77,7 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-@app.route("/accounts/<int:account_id>",methods=["GET"])
+@app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """ Reads an Account This endpoint will read an Account based the account_id that is requested """
     app.logger.info("Request to read an Account with id: %s", account_id)
